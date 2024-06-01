f41a98e172da272c79a77fee2aeb95188566d63f
lixiangwuxian <lixiangwuxian@lxtend.com>
2024-06-02 01:33:05 +0800

    build: add workflow support

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -1,3 +1,6 @@
+Param(
+    [String]$qmodname = "discord-presence"
+)
 # Builds a .qmod file for loading with QuestPatcher or BMBF
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
@@ -6,8 +9,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "discord-presence_v0.3.5.qmod"
-$TempArchiveName = "discord-presence_v0.3.5.qmod.zip"
+$ArchiveName = "$qmodname.qmod"
+$TempArchiveName = "$qmodname.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 Compress-Archive -Path "./libs/arm64-v8a/libdiscord-presence.so", "./libs/arm64-v8a/libbeatsaber-hook_3_14_0.so", "./mod.json", "./module.json", "./cover.png" -DestinationPath $TempArchiveName -Force
