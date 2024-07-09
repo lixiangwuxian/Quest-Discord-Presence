dd022d574ab81f65180a0f67f231dd2d779cc06b
lixiangwuxian <lixiangwuxian@lxtend.com>
2024-07-10 00:52:34 +0800

    init: try to refactor code and build system

--- build.ps1
+++ build.ps1
@@ -5,4 +5,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+# & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -B ./build .
+& cmake --build ./build
+
